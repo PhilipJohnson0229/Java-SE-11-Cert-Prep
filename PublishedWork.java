65416e31749dce16c400c1f2f5cf81a50526fdba
philipjohnson0229 <philip.johnson0229@gmail.com>
2022-05-08 23:52:11 -0400

    encapsulation and polymorphism review

--- PublishedWork.java
+++ PublishedWork.java
@@ -1,2 +1,22 @@
-package com.philipJohnson;public class PublishedWork {
+package com.philipJohnson;
+
+public class PublishedWork {
+    private String title;
+    private String author;
+
+    // default constructor
+    public PublishedWork() {
+        this.title = "no title";
+        this.author = "no name";
+    }
+
+    // overloaded constructor
+    public PublishedWork(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // accessors
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
 }
