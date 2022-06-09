a80379ce1c72dfd1fcfc005a55059edec01a84f5
philipjohnson0229 <philip.johnson0229@gmail.com>
2022-06-09 00:51:28 -0400

    generics workshop nearly complete

--- GenericsMain.java
+++ GenericsMain.java
@@ -2,6 +2,36 @@ package com.philipJohnson;
 
 public class GenericsMain {
     public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("joe");
+        BaseballPlayer jeff = new BaseballPlayer("jeff");
+        SoccerPlayer beckham = new SoccerPlayer("beckham");
+
+        Team<FootballPlayer> jaxWacksons = new Team<>("jaxWacksons");
+
+        jaxWacksons.addPlayer(joe);
+        //jaxWacksons.addPlayer(jeff);
+
+
+        System.out.println(jaxWacksons.numPlayer());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Cubs");
+        baseballTeam.addPlayer(jeff);
+
+        Team<SoccerPlayer> soccerPlayerTeam = new Team<>("Roaches");
+        soccerPlayerTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn= new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle= new Team<>("Fremantle");
+
+        hawthorn.matchScore(fremantle, 1, 0);
+        hawthorn.matchScore(jaxWacksons, 3, 8);
+
+        jaxWacksons.matchScore(fremantle, 2, 1);
+        jaxWacksons.matchScore(baseballTeam, 1, 1);
 
     }
 }
--- Team.java
+++ Team.java
@@ -1,8 +1,8 @@
 package com.philipJohnson;
 
 import java.util.ArrayList;
-
-public class Team {
+//the diamond with the T in the middle helps us declare generic types
+public class Team<T extends Player> {
     private String name;
 
     int played = 0;
@@ -10,7 +10,7 @@ public class Team {
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
@@ -20,16 +20,18 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player)
+    public boolean addPlayer(T player)
     {
         if(members.contains(player))
         {
+            //on this line were casting because java does not know if the player object has a getName method
+            //we know that player objects do have the method and we cast because we expect to find player objects
             System.out.println(player.getName() + " is already on the teamn");
             return false;
         }else
         {
             members.add(player);
-            System.out.println(player.getName() + " has been selected for the team " + this.getName());
+            System.out.println(((Player) player).getName() + " has been selected for the team " + this.getName());
         }
 
         return true;
@@ -42,24 +44,35 @@ public class Team {
 
     public void matchScore(Team opponent, int ourScore, int theirScore)
     {
+        String message;
+
         if(ourScore > theirScore)
         {
             won++;
+            message = " beat ";
         }
         else if(ourScore == theirScore)
         {
             tied++;
+            message = " drew with ";
         }
         else
         {
             lost++;
+            message = " lost to ";
         }
 
         played++;
 
         if(opponent != null)
         {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchScore(null, theirScore, ourScore);
         }
+    }
+
+    public int ranking()
+    {
+        return (won * 2) + tied;
     }
 }
